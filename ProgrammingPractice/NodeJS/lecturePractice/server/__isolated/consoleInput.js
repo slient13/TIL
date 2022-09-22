8ab1dccfcea3d6ddfaedbd672d0e89f870a898d7
slient13 <sjyhc25@gmail.com>
2022-09-22 21:09:39 +0900

    move practice files to outside (for obsidian optimizing)

--- ProgrammingPractice/NodeJS/lecturePractice/server/__isolated/consoleInput.js
+++
@@ -1,6 +0,0 @@
-var args = process.argv;
-args ? console.log(args) : console.log("None console input");
-console.log("");
-console.log("first arg: path of nodejs");
-console.log("second arg: path of file is called");
-console.log("third and mode args: argment what developer insert on console.");
--- ProgrammingPractice/p5Js/sketch.js
+++
@@ -1,6 +0,0 @@
-function setup() { // 시작 후 처음 한 번만 실행될 코드들
-    createCanvas(600, 400);
-    background('gray');
-
-    circle(300, 30, 50);
-}
